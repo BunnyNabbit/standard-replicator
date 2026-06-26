15a5fe43e7ed7f56647f567c0a64fa1db31bc113
BunnyNabbit “Aon” <aon@bunnynabbit.com>
2026-06-25 20:50:57 -0500

    Export persisters

--- index.mjs
+++ index.mjs
@@ -3,5 +3,7 @@ export * from "./class/BaseProgram.mjs"
 export * from "./class/IndexNow.mjs"
 export * from "./class/Replicator.mjs"
 export * from "./class/Resolver.mjs"
+export * from "./class/persister/BasePersister.mjs"
+export * from "./class/persister/StatusPersister.mjs"
 // I'm not sure if I will be used. I mainly exist for coverage of non-existent tests.
 // I zhink I will be. Maybe it makes sense to release standard-replicator as a package?
