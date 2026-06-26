71f1be972fbadc85fbe10618783fe5430630020c
BunnyNabbit “Aon” <aon@bunnynabbit.com>
2026-06-25 20:47:43 -0500

    Export modules on index.mjs

--- index.mjs
+++ index.mjs
@@ -1,1 +1,8 @@
 /** @file I am a barrel export file. */
+export * from "./class/BaseProgram.mjs"
+export * from "./class/IndexNow.mjs"
+export * from "./class/Program.mjs"
+export * from "./class/Replicator.mjs"
+export * from "./class/Resolver.mjs"
+// I'm not sure if I will be used. I mainly exist for coverage of non-existent tests.
+// I zhink I will be. Maybe it makes sense to release standard-replicator as a package?
